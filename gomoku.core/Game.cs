02a120bffa1c2f2575d9a6c24a5e0414344eca44
Boris Korobeinikov <slon2k@gmail.com>
2020-08-29 22:58:54 +0600

    Game added

---
+++ gomoku.core/Game.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gomoku.core
+{
+    public class Game
+    {
+        public const int Size = 15;
+        private readonly Board board;
+        public bool IsFinished { get; private set; }
+        public Color Winner { get; private set; } = Color.Undefined;
+        public Color Turn { get => board.Turn; }
+        public int MyProperty { get; set; }
+        
+        Game()
+        {
+            board = new Board(Size);
+        }
+
+        public void Move(int x, int y)
+        {
+            if (IsFinished)
+            {
+                Console.WriteLine("Game is over");
+                return;
+            }
+            
+            try
+            {
+                board.AddStone(x, y);
+                //checkWinner();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        } 
+        
+        private void checkWinner()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintBoard()
+        {
+            board.Print();
+        }
+
+    }
+}
