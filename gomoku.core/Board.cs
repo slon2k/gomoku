d650fc8df5654230c977da083c07e6859beaef07
Boris Korobeinikov <slon2k@gmail.com>
2020-08-29 20:00:53 +0600

    Add project files.

---
+++ gomoku.core/Board.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace gomoku.core
+{
+    public class Board
+    {
+        public readonly int Size;
+        private readonly Color[,] Position;
+        public Color Turn { get; private set; } = Color.Black;
+
+        public Board(int size)
+        {
+            Size = size;
+            Position = new Color[Size, Size];
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    Position[i, j] = Color.Undefined;
+                }
+            }
+        }
+
+        public Board(Board board)
+        {
+            Size = board.Size;
+            Position = new Color[Size, Size];
+            Turn = board.Turn;
+        }
+
+        public void AddStone(int x, int y)
+        {
+            if (x < 0 || y < 0 || x > Size - 1 || y > Size - 1 )
+            {
+                throw new ArgumentOutOfRangeException("Position does not exist");
+            }
+
+            if (Position[x, y] != Color.Undefined)
+            {
+                throw new Exception("The cell is not empty");
+            }
+
+            Position[x, y] = Turn;
+            Turn = Turn.Inverse();
+        }
+
+        public Color Cell(int x, int y)
+        {
+            if (x < 0 || y < 0 || x > Size - 1 || y > Size - 1)
+            {
+                throw new ArgumentOutOfRangeException("");
+            }
+            
+            return Position[x, y];
+        }
+
+        public IList<Cell> Cells
+        {
+            get
+            {
+                var cells = new List<Cell>();
+                for (int i = 0; i < Size; i++)
+                {
+                    for (int j = 0; j < Size; j++)
+                    {
+                        cells.Add(new Cell(i, j, Position[i, j]));
+                    }
+                }
+                return cells;
+            }           
+        }
+
+        public IList<Cell> FreeCells
+        {
+            get
+            {
+                return Cells.Where(c => c.color == Color.Undefined).ToList();
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                var str = new StringBuilder();
+                for (int j = 0; j < Size; j++)
+                {
+                    str.Append(Cell(i, j).PrintColor());
+                }
+                Console.WriteLine(str);
+            }
+        }
+    }
+
+    public struct Cell
+    {
+        public int x;
+        public int y;
+        public Color color;
+
+        public Cell(int x, int y, Color color)
+        {
+            this.x = x;
+            this.y = y;
+            this.color = color;
+        }
+    }
+
+    public enum Color : byte
+    {
+        Undefined,
+        Black,
+        White
+    }
+}
---
+++ gomoku.core/ColorExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gomoku.core
+{
+    public static class ColorExtensions
+    {
+        public static Color Inverse(this Color color)
+        {
+            switch (color)
+            {
+                case Color.White: return Color.Black;
+                case Color.Black: return Color.White;
+                default: return Color.Undefined;
+            }
+        }
+
+        public static string PrintColor(this Color color)
+        {
+            switch (color)
+            {
+                case Color.Black: return " X ";
+                case Color.White: return " 0 ";
+                default: return " _ ";
+            }
+        }
+    }
+}
